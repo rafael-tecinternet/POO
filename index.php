2eab6c6ffbd63deeb588471779a4744269c87519
rafael-tecinternet <rafah1194@gmail.com>
2022-06-08 09:19:04 -0300

    Acesso direto aos recursos
    Atribuição e leitura de dados em propriedades e métodos públicos

--- index.php
+++ index.php
@@ -4,24 +4,42 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo 2</title>
+    <title>Exemplo 3</title>
 </head>
 <body>
-    <h1>PHP POO - Exemplo 2</h1>
+    <h1>PHP POO - Exemplo 3</h1>
     <hr>
     <h2>Assuntos abordados:</h2>
     <ul>
-        <li>Criação dos objetos</li>
-        <li>Uso do método construtor</li>
-        <li>Uso do <code>$this</code> para acessar as propriedades</li>
+        <li>Acesso direto à propriedades</li>
+        <li>Atribuição de dados e leitura</li>
     </ul>
 <?php
 // Importando a classe
 require_once "src/Clinte.php";
-// Criação dos objetos
-$clienteA = new Cliente('Rafael', 'rafah93@gmail.com');
-$clienteB = new Cliente('Jon Oliva', 'savatage@msn.com');
+$clienteA = new Cliente;
+$clienteB = new Cliente;
+// Atribundo dados às propriedades do objeto
+$clienteA->nome = "Rafael";
+$clienteA->email = "rafael@gmail.com";
+$clienteA->telefones = ["11-2345-5432, 11-98766-4444"];
+$clienteB->nome = "Bernardo";
+$clienteB->email = "bernardo@gmail.com";
+$clienteB->telefones = array("11-95556-9895");
 ?>
-<pre><?=var_dump($clienteA, $clienteB)?></pre>
+<h2>Dados dos objetos (leitura)</h2>
+<h3><?=$clienteA->nome?></h3>
+<p>E-mail: <?=$clienteA->email?></p>
+<p>Telefones: <?=implode(", ",$clienteA->telefones)?></p>
+<p>Senha: <?=$clienteA->senha?></p>
+
+<h3><?=$clienteB->nome?></h3>
+<p>E-mail: <?=$clienteB->email?></p>
+<p>Telefones: <?=implode(", ",$clienteB->telefones)?></p>
+<p>Senha: <?=$clienteB->senha?></p>
+
+<h2>Chamando método exibirDados</h2>
+<?=  $clienteA->exibirDados()?>
+<?=  $clienteB->exibirDados()?>
 </body>
 </html>
